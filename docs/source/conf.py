6e0dc775507cf7f49e59108b184bccf69212e56f
Alex Aleev <aleev@simicon.com>
2025-02-27 13:08:45 +0300

    docs: add sphinx settings for base generator

---
+++ docs/source/conf.py
@@ -0,0 +1,52 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Codestral ROS2 Generator"
+copyright = "2025, Lexmaister"
+author = "Lexmaister"
+release = "0.2"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+import sys
+from codestral_ros2_gen import get_project_root
+
+sys.path.insert(0, get_project_root())  # Add project root to path
+
+# Extensions
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinx_autodoc_typehints",
+    "myst_parser",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+
+# Theme configuration
+html_theme = "alabaster"  # default
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+
+# Napoleon settings for docstring parsing
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = True
+
+# autodoc configuration
+autodoc_member_order = "bysource"
+autodoc_typehints = "description"
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. Codestral ROS2 Generator documentation master file, created by
+   sphinx-quickstart on Thu Feb 27 12:33:55 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Codestral ROS2 Generator's documentation!
+====================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   generators/base_generator
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
